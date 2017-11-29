3c915f5272a73b5cdf3afbbeaa26148bee122298
dobrosi <dobrosi.andras@hmei.hu>
2017-11-29 13:47:46 +0100

    fixes

--- src/main/java/p9yp9y/vaos/VaosApplication.java
+++ src/main/java/p9yp9y/vaos/VaosApplication.java
@@ -1,6 +1,9 @@
 package p9yp9y.vaos;
 
+import com.vaadin.server.Resource;
+
 public interface VaosApplication {
 	public void main(String[] args);
-	public String getName();
+	public String getApplicationName();
+	public Resource getApplicationIcon();
 }
