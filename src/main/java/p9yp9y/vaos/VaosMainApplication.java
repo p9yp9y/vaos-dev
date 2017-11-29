0817b77699c1dbec2e48ae2ecf9d2da78746ce51
dobrosi <dobrosi.andras@hmei.hu>
2017-11-29 15:43:18 +0100

    fix

---
+++ src/main/java/p9yp9y/vaos/VaosMainApplication.java
@@ -0,0 +1,10 @@
+package p9yp9y.vaos;
+
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+import java.lang.annotation.Retention;
+
+@Retention(CLASS)
+public @interface VaosMainApplication {
+
+}
